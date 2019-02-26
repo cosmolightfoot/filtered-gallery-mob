4627c372b94df4abc799ecfa6cb83745404c3e9b
Cosmo Kent <cosmo.lightfoot@gmail.com>
2019-02-26 14:39:19 -0800

    form inputs created, began testing for filtered image function

--- test/file2.test.js
+++ test/file2.test.js
@@ -1,7 +1,51 @@
 const test = QUnit.test;
 
-QUnit.module('test suite two');
+QUnit.module('image filter test');
+function filterImages(images, filter) {
+    //filter method
+    //return objects in an array
+}
+test('image filter test', function (assert) {
+    //arrange
+    const images = [
+        {
+            'keyword': 'chameleon',
+            'horns': 2
+        },
+        {
+            'keyword': 'unicorn',
+            'horns': 6
+        },
+        {
+            'keyword': 'chameleon',
+            'horns': 4
+        }
+    ];
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+
+    const filter = {
+        keyword: 'chameleon'
+    };
+
+
+    
+    // object with arrays in it
+    // test function
+    
+    //act
+    const filteredImage = filteredImage(images, filter);
+    const expected = [
+        {
+            'keyword': 'chameleon',
+            'horns': 2
+        },
+        {
+            'keyword': 'chameleon',
+            'horns': 4
+        }
+    ];
+
+    //assert
+
+    assert.deepEqual(filteredImage, expected);
 });
