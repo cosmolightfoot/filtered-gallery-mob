22414aed7baa533525646371942c7d74247e2fce
Cosmo Kent <cosmo.lightfoot@gmail.com>
2019-02-26 17:28:11 -0800

    finished for the day

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import images from '../data/images.js';
 import buildGallery from './build-gallery.js';
 import filteredImage from './filter-images.js'; 
-
-const imageListNode = document.getElementById('image-list');
-images.forEach(function(images) {
-    const dom = buildGallery(images);
-    imageListNode.appendChild(dom);
-});
 const formNode = document.getElementById('filter-list');
 
 formNode.addEventListener('submit', event => {
     event.preventDefault();
+    const imageListNode = document.getElementById('image-list');
+    console.log(imageListNode.children);
 
     const formData = new FormData(formNode);
     const filter = {
         keyword: formData.get('keyword'),
         horns: formData.get('horns')
     };
-
-    const filteredResult = filteredImage(images, filter);
-    console.log(filteredResult);
     
+    const filteredResult = filteredImage(images, filter);
+    const imageChildren = imageListNode.children;
+    while(imageChildren.length > 0) {
+        imageListNode.removeChild();
+    }
+
+    filteredResult.forEach(function(filteredResult) {
+        const dom = buildGallery(filteredResult);
+        imageListNode.appendChild(dom);
+    });
 });
 
 
